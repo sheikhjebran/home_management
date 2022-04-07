7b6cedae19ae62e236b4426d457510230d78f546
jebran.sheikh@philips.com <sheikhjebran@gmail.com>
2022-04-07 05:42:27 +0530

    push my file

--- HomE/HomEApp/urls.py
+++ HomE/HomEApp/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('index.html', views.home, name="home"),
     path('login.html', views.login, name="login"),
     path('authenticate', views.authenticate, name="authenticate"),
-
+    path('rent', views.rent_detail, name="rent_detail"),
+    path('tenent', views.tenent_detail, name="tenent_detail"),
+    path('home_detail', views.home_detail, name="home_detail"),
 ]
